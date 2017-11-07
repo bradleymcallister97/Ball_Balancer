626912b1a63942e019c86b63d7eed963f130c459
Raj M <rajtfib@hotmail.ca>
2017-11-07 12:59:02 -0500

    Python Serial Data Collector

---
+++ main/SerialRead.py
@@ -0,0 +1,14 @@
+import Serial
+
+ser = serial.Serial('COM9', 9600, timeout =1)
+
+while 1:
+    try:
+        print ser.readline()
+        time.sleep(1)
+    except ser.SerialTimeoutException:
+        print('Data could not be read \r\n')
+        time.sleep(1)
+
+    data = ser.readline()
+    # PARSE SERIAL DATA FROM HERE
