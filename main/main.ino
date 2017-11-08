4fdb06b5d12e636766429b4416c0c9b81e5503df
bradleymcallister97 <brad.mcallister@hotmail.com>
2017-11-08 17:45:01 -0500

    clean main file

--- main/main.ino
+++ main/main.ino
@@ -2,7 +2,7 @@
 
 Servo servo;
 
-#define RUN_TEST
+//#define RUN_TEST
 #define RUN_PID
 
 // Pin locations
@@ -10,15 +10,22 @@ const int servoPin = 9;
 const int trigPin = 11;
 const int echoPin = 10;
 
+// SET 1
+//const float Kp = 5;
+//const float Ki = 3;
+//const float Kd = -3.3;
+// SET 2
+//const float Kp = 4.9;
+//const float Ki = 2.9;
+//const float Kd = -2.15;
+
 // Constants for PID algorithm
-const float Kp = 8;
-const float Ki = 6;
-const float Kd = -5;
+const float Kp = 6;
+const float Ki = 4.75;
+const float Kd = -3;
 
 // Global variables
-double middlePoint = 15;
-double middlePoint_Upper = 1;
-double middlePoint_Lower = 1;
+double setPoint = 15;
 double intError = 0;
 const unsigned long pollingTime = 10;
 float pollingSecs = pollingTime/1000;
@@ -42,7 +49,7 @@ void loop(){
 
 #endif
 
-#ifndef RUN_PID
+#ifdef RUN_PID
     long now = millis();
     if (now - lastTimeCalculated >= pollingTime){
         int ballLocation = getBallLocation();
@@ -80,14 +87,7 @@ void setup(){
 }
 
 int getBallLocation(){
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    int duration = pulseIn(echoPin, HIGH);
+    int duration = getSonarValue();
     // Calculating the distance
     int distance = duration*0.034/2;
     if (distance < 0) distance = 51;
@@ -95,14 +95,25 @@ int getBallLocation(){
     return distance;
 }
 
+int getSonarValue(){
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    return pulseIn(echoPin, HIGH);
+}
+
 int calculate(double input){
-    double error = middlePoint - input;
+    double error = setPoint - input;
 //    Serial.println(error);
     intError += error * pollingSecs;
     double derError = (error - lastError);
 
     double output = error * Kp + intError * Ki + derError * Kd;
-    if (output > 25) output = 25;
+    if (output > 33) output = 33;
     else if (output < 0) output = 0;
 
     if (abs(output - lastOutput) < 5) output = lastOutput;
@@ -111,27 +122,19 @@ int calculate(double input){
     return output;
 }
 
-float calculate_ball_speed(void)
+float calculate_ball_speed()
 {
     unsigned long time_now = sound_travel_time();
-    float distance_cm = time/29/2;
+    float distance_cm = time_now/29/2;
     float speed = (distance_cm - last_distance_cm) / (time_now - last_read_time);
     last_distance_cm = distance_cm;
     last_read_time = time_now;
     return speed;
 }
 
-long sound_travel_time(void)
+long sound_travel_time()
 {
-    unsigned long now = millis();
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    uint32_t duration = pulseIn(echoPin, HIGH);
+    uint32_t duration = getSonarValue();
     last_read_time = now;
     if( duration == 0)
     {
